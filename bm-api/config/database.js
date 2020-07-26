5aca48728f185a54008a13399536e7492379301d
RJIndu <indu.anya@gmail.com>
2020-07-26 23:25:41 +0530

    changes to connect mongo

--- bm-api/config/database.js
+++ bm-api/config/database.js
@@ -1,8 +1,19 @@
 //Set up mongoose connection
+const config = require('../config.json');
 console.log('MongoDB config');
 const mongoose = require('mongoose');
-const mongoDBUri = 'mongodb+srv://indu:indu123@cluster0-bjksa.mongodb.net/begemart?retryWrites=true&w=majority';
-mongoose.connect(mongoDBUri,{ useNewUrlParser: true, useUnifiedTopology: true });
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions)
+.then(() => {
+    console.log('connected to MONGO  DB successfully');
+    console.log(mongoose.connection.host+":"+mongoose.connection.port);
+ })
+.catch(err => {
+    console.log('connection failed ' + err);
+}); 
+// const mongoDBUri = 'mongodb+srv://indu:indu123@cluster0-bjksa.mongodb.net/begemart?retryWrites=true&w=majority';
+// mongoose.connect(mongoDBUri,{ useNewUrlParser: true, useUnifiedTopology: true })
+    
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
